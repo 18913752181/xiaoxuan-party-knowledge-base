9bf8d879b05c024d3b616db21f5422e837f4d56d
宣知 <1743398170@qq.com>
2026-07-19 23:06:19 +0800

    restore: local site snapshot before style changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         brand: {
           red: "#8f1d22",
           darkRed: "#651419",
-          ink: "#4b4f4d",
+          ink: "#2f3430",
           gray: "#f7f4ee",
           line: "#e7e1d8",
           paper: "#fbf8f1",
