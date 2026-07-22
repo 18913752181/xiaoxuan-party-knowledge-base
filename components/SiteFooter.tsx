c667cb2a2f56c5d392573108658beefa0f135379
宣知 <1743398170@qq.com>
2026-07-22 22:56:43 +0800

    content: add latest materials and ICP filing

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -4,6 +4,7 @@
       <div className="mx-auto flex max-w-6xl flex-col gap-2 px-5 py-8 text-sm text-neutral-500 lg:px-8">
         <p>© 2026 宣知 · 苏州宣知科教发展有限公司</p>
         <p>把常用的党建资料，清楚地整理在一起。</p>
+        <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer" className="w-fit transition hover:text-[#9a4650]">苏ICP备2026050150号</a>
       </div>
     </footer>
   );
